77676ff229233f9ac92a4707fd505a9b2afd70d5
Shivam <shivamdhiman336@gmail.com>
2025-11-04 16:23:36 +0530

    fixed path issue for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/', // 👈 ye line add karni hai
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
